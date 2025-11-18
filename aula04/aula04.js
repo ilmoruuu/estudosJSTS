e23edfb77d27a24fe3cdd357660c2dcd2a465b28
Murilo Lucena <muriloglucenacanal@gmail.com>
2025-11-18 11:40:52 -0300

    feat:. conclusão da aula de operadores matemáticos

---
+++ aula04/aula04.js
@@ -0,0 +1,12 @@
+//Operadores Matemáticos
+
+//inicialização de todas as variáveis
+//let num1=num2 = 10
+
+let num1=0,num2=0,res=0
+num1=15
+num2=2
+// +, - , * , / , % , ** , ++, --
+res=++num1
+
+console.log(res)
