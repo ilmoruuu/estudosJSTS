0b3fb982a4ceb9f2d9e1fd046559219bf2cd4183
Murilo Lucena <muriloglucenacanal@gmail.com>
2025-12-12 20:39:34 -0300

    feat: conclusão da aula do loop for

---
+++ aula15/aula15.js
@@ -0,0 +1,7 @@
+//for ( [expressão inicial]; [expressão condicional]; [atualização da expressão inicial] ) {
+	// um ou vários comandos a serem executados //
+	// declaração; //
+﻿
+for(let i = 0; i < 5; i++) {
+    console.log(`Número ${i}`)
+}
